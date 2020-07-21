2d1d19fe27c28f09913900ab76c63b715713a999
Kamal Hamza <khamsa.kamal48@gmail.com>
2020-07-21 23:55:47 +0530

    Added validation to check length of phone numbers & send API requests

--- Verify_Numbers.sh
+++ Verify_Numbers.sh
@@ -14,11 +14,11 @@ number_length=$(echo "${#number}")
 access_key=$(jq -r '.access_key' API.json)
 
 #Verify based on the length of mobile number
-if [[ "$number_length" -ge 10 ]]; then
+if [[ "$number_length" == 10 ]]; then
   #Mobile No. is of 10 digits.
   curl -s "http://apilayer.net/api/validate?access_key=${access_key}&number=${number}" 2>&1 | tee Number_Verify_Output.json;
 else
-  #Mobile No. is less than 10 digits.
+  #Mobile No. is not of 10 digits.
   curl -s "http://apilayer.net/api/validate?access_key=${access_key}&number=${number}&country_code=IN" 2>&1 | tee Number_Verify_Output.json;
 fi
 
